30b09d42a599faf6ffdf3ff1d2fd31b6bd4d53e1
Jake Rosoman <jkroso@gmail.com>
2013-09-04 00:11:19 +1200

    expose the deferred constructor

--- defer.js
+++ defer.js
@@ -8,30 +8,34 @@ var then = Result.prototype.then
 var read = Result.prototype.read
 
 /**
- * create a DeferredResult which is associated with
- * procedure `ƒ`. `ƒ` will only be evaluated once
- * someone actually reads from the DeferredResult.
- *
- * @param {Function} ƒ (write, error)
- * @return {DeferredResult}
+ * the Deferred class
  */
 
-module.exports = function(ƒ){
-	var deferred = new DeferredResult
-	deferred.ƒ = ƒ
-	return deferred
+function Deferred(fn){
+	this.onNeed = fn
 }
 
-inherit(DeferredResult, Result)
+/**
+ * inherit from Result
+ */
 
-function DeferredResult(){}
+inherit(Deferred, Result)
 
-DeferredResult.prototype.i = 0
-DeferredResult.prototype.state = 'awaiting'
-DeferredResult.prototype.then = trigger(then)
-DeferredResult.prototype.read = trigger(read)
-DeferredResult.prototype.write = unawait(write)
-DeferredResult.prototype.error = unawait(error)
+Deferred.prototype.i = 0
+Deferred.prototype.state = 'awaiting'
+Deferred.prototype.then = trigger(then)
+Deferred.prototype.read = trigger(read)
+Deferred.prototype.write = unawait(write)
+Deferred.prototype.error = unawait(error)
+
+/**
+ * add a trigger aspect to `method`. This aspect
+ * ensures `onNeed` is called on first read().
+ *
+ * @param {Function} method
+ * @return {Function}
+ * @api private
+ */
 
 function trigger(method){
 	return function(onValue, onError){
@@ -40,11 +44,11 @@ function trigger(method){
 			try {
 				var self = this
 				var val
-				if (this.ƒ.length) {
-					this.ƒ(
+				if (this.onNeed.length) {
+					this.onNeed(
 						function(val){ self.write(val) },
 						function(err){ self.error(err) })
-				} else if ((val = this.ƒ()) !== undefined) {
+				} else if ((val = this.onNeed()) !== undefined) {
 					if (val instanceof ResultType) {
 						val.read(
 							function(val){ self.write(val) },
@@ -61,9 +65,51 @@ function trigger(method){
 	}
 }
 
+/**
+ * add a state switching aspect to `method`.
+ * Otherwise write/error would behave incorrectly
+ *
+ * @param {Function} method
+ * @return {Function}
+ * @api private
+ */
+
 function unawait(method){
 	return function(value){
 		if (this.state === 'awaiting') this.state = 'pending'
 		return method.call(this, value)
 	}
 }
+
+/**
+ * create a Deferred which is associated with the
+ * Function `onNeed`. `onNeed` will only be called
+ * once someone actually reads from the Deferred.
+ *
+ *   defer(function(){
+ *     this.write('hello')
+ *   })
+ *
+ *   defer(function(write){
+ *     write('hello')
+ *   })
+ *
+ * @param {Function} onNeed(write, error)
+ * @return {Deferred}
+ */
+
+module.exports = exports = function(onNeed){
+	return new Deferred(onNeed)
+}
+
+/**
+ * share prototypes
+ */
+
+exports.prototype = Deferred.prototype
+
+/**
+ * expose Deferred
+ */
+
+exports.Deferred = Deferred
